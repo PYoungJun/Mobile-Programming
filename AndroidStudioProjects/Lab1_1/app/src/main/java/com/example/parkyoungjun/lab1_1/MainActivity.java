b934ca3a1b4fc16c2f8907cb9e5446485b1a7370
YJ <marshmello37@naver.com>
2018-04-10 12:15:25 +0900

    Lab2-2

--- AndroidStudioProjects/Lab1_1/app/src/main/java/com/example/parkyoungjun/lab1_1/MainActivity.java
+++ AndroidStudioProjects/Lab1_1/app/src/main/java/com/example/parkyoungjun/lab1_1/MainActivity.java
@@ -14,25 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView=(ImageView)findViewById(R.id.bar);            //match id which is bar
-        imageView2=(ImageView)findViewById(R.id.real);           //match id which is real
+        imageView=(ImageView)findViewById(R.id.bar);            /**match id which is bar*/
+        imageView2=(ImageView)findViewById(R.id.real);           /**match id which is real*/
     }
     public void onButton1Clicked(View v){
-        changeImage();              //the event is occured when clicked the button
+        changeImage();              /**the event is occured when clicked the button*/
     }
     private void changeImage(){
         if(imageIndex==0){
-            imageView.setVisibility(View.VISIBLE);      //if imageindex is 0 set image imageview
-            imageView2.setVisibility(View.INVISIBLE);   //and set imageindex 1
+            imageView.setVisibility(View.VISIBLE);      /**if imageindex is 0 set image imageview*/
+            imageView2.setVisibility(View.INVISIBLE);   /**and set imageindex 1*/
 
             imageIndex=1;
         }
         else if(imageIndex==1)
         {
-            imageView.setVisibility(View.INVISIBLE);        //if imageindex is 1 set image imageview
-            imageView2.setVisibility(View.VISIBLE);         //and set imageindex 0
+            imageView.setVisibility(View.INVISIBLE);        /**if imageindex is 1 set image imageview*/
+            imageView2.setVisibility(View.VISIBLE);         /**and set imageindex 0*/
 
             imageIndex=0;
         }
+        //https://github.com/PYoungJun/MP_lab1-2.git
     }
 }
